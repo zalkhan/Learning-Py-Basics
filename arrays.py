c4b6cb7107c107118094f5e249c585fb93a2135b
Zaydan AlKahn <52993650+zalkhan@users.noreply.github.com>
2022-06-02 21:41:07 +0400

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,15 @@
+# arrays are a data structure similar to lists.
+# they contain multiple values or elements in a single variable.
+# in python, there is no built-in array support like c# or c++.
+# so we can represent them as lists instead.
+
+children = ['James', 'Violet', 'Belle']
+x = children[0]
+print(x)
+
+# this is similar to looping through tuples and lists by specifying our index search.
+# lets try changing values of a given array.
+
+children = ['James', 'Violet', 'Belle']
+children[0] = 'Alice'
+print(children)
