b514ac2e74da909e2b931c93a100173c29055b8b
Zaydan AlKahn <52993650+zalkhan@users.noreply.github.com>
2022-06-05 20:55:25 +0400

    Add files via upload

---
+++ dates.py
@@ -0,0 +1,6 @@
+# A date in Python is not a data type of its own, but we can import a module named datetime to work with dates as date objects.
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
