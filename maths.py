31e60f011f1d3be2bb8f31625294083c5250ff32
Zaydan AlKahn <52993650+zalkhan@users.noreply.github.com>
2022-06-07 21:00:00 +0400

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,31 @@
+# The min() and max() functions can be used to find the lowest or highest value in an iterable
+
+x = min(5, 10, 15)
+y = max(20, 25, 30)
+print (x)
+print (y)
+
+# there is also a pythonmodule called minimax which is an algorithm used in AI's, for instance chess AI.
+
+# abs function calculates the absolute value in positive version.
+
+x = abs(-9.5)
+print (x)
+
+# we can use exponential notation as well.
+z = pow(10, 2)
+print (z)
+
+# lets import the math module for python
+
+import math
+
+v = math.sqrt(700)
+print(v)
+
+# The math.ceil() method rounds a number upwards to its nearest integer. 
+# the math.floor() method rounds a number downwards to its nearest integer.
+zab = math.ceil(3.14)
+xab = math.floor(3.14)
+print(zab)
+print(xab)
