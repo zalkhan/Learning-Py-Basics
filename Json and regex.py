eb771785f801d47e27e101c31df4cc0de3b1aefb
Zaydan AlKahn <52993650+zalkhan@users.noreply.github.com>
2022-06-14 18:32:15 +0400

    Update Json and regex.py
    updated methods for RE

--- Json and regex.py
+++ Json and regex.py
@@ -80,3 +80,13 @@ if x:
     print('Matched')
 else:
     print('null')
+'''
+The re module offers a set of functions that allows us to search a string for a match:
+
+Function	Description
+findall	Returns a list containing all matches
+search	Returns a Match object if there is a match anywhere in the string
+split	Returns a list where the string has been split at each match
+sub	Replaces one or many matches with a string
+
+'''
